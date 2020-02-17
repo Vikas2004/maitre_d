47630f58fcb605cf230729010c5b1e88705e5c76
Vikas2004 <S538336@NWMISSOURI.EDU>
2020-02-17 11:21:09 -0600

    javascript commit

---
+++ final.js
@@ -0,0 +1,16 @@
+const total = document.querySelector('#total');
+const btn = document.querySelector('#btn');
+const tip = document.querySelector('#tip');
+const rate = document.querySelector('#rate');
+const result = document.querySelector('#result');
+btn.addEventListener("click",calculate);
+
+
+function calculate()
+{
+    const tax = 0.055;
+    
+    tip.textContent='Tip: $' + ((rate.value*total.value)/100.0);
+    calctotal = (rate.value*total.value/100.0)+Number(total.value)+(tax*total.value);
+    result.textContent = 'Your Total is: $'+calctotal;
+}
